a6f828d7c3bd5e7e8bd57057409427ee7c9d6a12
Arpit <ghatearpit@gmail.com>
2020-12-28 22:09:45 +0530

    Sum of two no

---
+++ AddTwoNo.java
@@ -0,0 +1,8 @@
+public class AddTwoNo {
+    public static void main(String[] args) {
+         int num1=5 , num2=15, sum;
+	sum = num1 + num2;
+
+	System.out.println("SUm of these number: "+sum);
+}
+}
