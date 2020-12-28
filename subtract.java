be1b028ecd2093f16b47302e2e987a93f1a5f600
Arpit <ghatearpit@gmail.com>
2020-12-28 22:35:42 +0530

    subtraction of two no

---
+++ subtract.java
@@ -0,0 +1,8 @@
+public class subtract {
+    public static void main(String[] args) {
+         int num1=51 , num2=1, sum;
+	sum = num1 - num2;
+
+	System.out.println("Sub of these number: "+sum);
+}
+}
